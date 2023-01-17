ad10fc7e9f2a87d4f9d76aeaf22a24a242dd6e32
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-01-17 13:44:12 +0100

    Refactor: Split the structs from h file to a new file

---
+++ structs.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 13:41:13 by victofer          #+#    #+#             */
+/*   Updated: 2023/01/17 13:43:09 by victofer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTS_H
+# define STRUCTS_H
+# include "./push_swap.h"
+
+// ----------- E N U M S ---------------
+	// Enum with int limits
+enum e_int
+{
+	INT_LIMIT = 2147483647,
+	INT_LIMIT_NEG = -2147483648
+};
+
+	// enum with TRUE and FALSE (boolean)
+enum e_bool
+{
+	TRUE = 1,
+	FALSE = 0
+};
+
+#endif
