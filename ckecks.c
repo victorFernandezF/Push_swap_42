1cdb1eacfcca17a6538eddd2ef09286305cece39
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-01-17 13:36:27 +0100

    Docs: Add comments to clarify what does each function

--- ckecks.c
+++ ckecks.c
@@ -6,12 +6,13 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 11:48:56 by victofer          #+#    #+#             */
-/*   Updated: 2023/01/17 13:17:39 by victofer         ###   ########.fr       */
+/*   Updated: 2023/01/17 13:35:23 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./push_swap.h"
 
+// Checks if any value from args isn't numeric.
 int	check_bad_input(char *input)
 {
 	int	i;
@@ -23,6 +24,7 @@ int	check_bad_input(char *input)
 	return (FALSE);
 }
 
+// Checks if any numbers is bigger than int limit. (NOT WOKING YET)
 int	check_int_limit(int cant, char **input)
 {
 	int	i;
@@ -35,6 +37,7 @@ int	check_int_limit(int cant, char **input)
 	return (FALSE);
 }
 
+// Checks if any of the numbers is duplicated. 
 int	check_duplicated_numbers(int cant, int *numbers)
 {
 	int	i;
@@ -52,4 +55,31 @@ int	check_duplicated_numbers(int cant, int *numbers)
 		}
 	}
 	return (FALSE);
+}
+
+// Checks if user inputs the numbers already sorted.
+int	is_sorted(int cant, int *nb)
+{
+	int	i;
+
+	i = 0;
+	while (i < cant - 1)
+	{
+		if (nb[i] > nb[i + 1])
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
+// Call the checker functions and return error if needed.
+int	check_everything(int cant, char **argv, int *numbers)
+{
+	(void)argv;
+	if (check_duplicated_numbers(cant, numbers))
+		ft_error_free(numbers);
+	if (is_sorted(cant, numbers))
+		ft_printf("Sorted\n");
+	
+	return (0);
 }
