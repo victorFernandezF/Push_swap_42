6294e67f607c141798449dc9c4703e859732f0fb
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-02-07 19:23:04 +0100

    Feat: Initialize chunk_len

--- src/init_stack_data.c
+++ src/init_stack_data.c
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 12:11:28 by victofer          #+#    #+#             */
-/*   Updated: 2023/02/06 13:28:43 by victofer         ###   ########.fr       */
+/*   Updated: 2023/02/07 19:05:49 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,17 @@
 // Initializes datas from stack. Used in main
 void	init_stack(t_stack *stack)
 {
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
 	stack->b = (int *)malloc(1 * sizeof(int));
+	stack->chunk_1 = (int *)malloc(19 * sizeof(int));
+	if (!stack->chunk_1)
+		ft_error_free(*stack);
 	stack->a_len = 0;
+	stack->chunk_len = 20;
 	stack->pb_count = 0;
 	stack->b_len = 0;
 	stack->moves = 0;
